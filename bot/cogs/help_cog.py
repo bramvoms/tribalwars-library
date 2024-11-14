e27ac28e2828420806bc1aebc4cc70252f1f3786
bramvoms <78076949+bramvoms@users.noreply.github.com>
2024-11-14 21:43:59 +0100

    Update help_cog.py

--- bot/cogs/help_cog.py
+++ bot/cogs/help_cog.py
@@ -10,7 +10,7 @@ class HelpCog(commands.Cog):
     async def help_command(self, ctx):
         # Create an embed for the help menu
         embed = create_embed(
-            title=f"📜 TribalWars Library 📜",
+            title=f"📜 **━━━━━━ TribalWars Library ━━━━━━** 📜",
             description="This bot provides a range of commands and functionalities to enhance your experience. Below is a list of all available commands and features."
         )
 
