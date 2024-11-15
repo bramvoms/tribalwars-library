d3c3fd9234413da382555a681c7130fa22eec28d
bramvoms <78076949+bramvoms@users.noreply.github.com>
2024-11-15 17:41:07 +0100

    Update am_cog.py

--- bot/cogs/am_cog.py
+++ bot/cogs/am_cog.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 from discord import app_commands, Interaction
 from discord.ui import View, Button
 from fuzzywuzzy import process  # Import for fuzzy matching
-from deepl import Translator  # Import DeepL translator
+import deepl
 from main import create_embed
 
 # Initialize DeepL Translator with your API key
